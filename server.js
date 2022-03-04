2fbce7aa9a0a790b95bc3dea47b08beb46036485
Rahul Raj Miglani <rahul.cad2019@gmail.com>
2022-03-03 16:20:02 -0800

    server-client connection and event listenr

---
+++ server.js
@@ -0,0 +1,37 @@
+
+
+// Import Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+
+// Declare JSON body Parser
+const bodyParser = require('body-parser');
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Cors for cross origin allowance - it helps server and app talk without any interuptions
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder - to direct server to look into this folder
+app.use(express.static('website'));
+
+// Declare the port to be used
+const port = 8000;
+// Setup Server
+const server = app.listen(port, listening);
+
+// callback function
+function listening(){
+    console.log(`Server is running at localhost and port: ${port}`);
+}
+
+// ROUTES
+
+// POST Route
+
+
+// GET Route
---
+++ website/app.js
@@ -0,0 +1,14 @@
+// Global Variables
+
+const generate = document.querySelector('#generate');
+
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let currentDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+
+generate.addEventListener('click', (event) => {
+    event.preventDefault();
+
+
+})
